d73bda6092677d4ecf1bec7149a1e30a122c73fa
Aliblack <alibekept@mail.ru>
2023-07-13 20:56:17 +0600

    First Commit

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿//Вывод второго числа
+/*
+void ShowSecNumber ()
+{
+    Console.WriteLine("Enter your number");
+    int userNum = Convert.ToInt32(Console.ReadLine());
+    if (userNum > 100 && userNum < 1000)
+    {
+        int digit1 = userNum % 100;
+        int digit2 = digit1 / 10;
+        int newNum = digit2;
+        Console.WriteLine($"Result is {newNum}");
+    }
+    else Console.WriteLine("I CANT MY LORD");
+}
+ShowSecNumber ();
+*/
+/*
+//Вывод третьего числа
+void ShowThirdNumb ()
+{
+    Console.WriteLine("Enter your number");
+    int usNum = Convert.ToInt32(Console.ReadLine());
+    if (usNum > 100 && usNum < 1000)
+    {
+        int digit1 = usNum % 100;
+        int digit2 = digit1 % 10;
+        int newNum = digit2;
+        Console.WriteLine($"Result is {newNum}");
+    }
+    else Console.WriteLine("We haven't third Num MY LORD");
+
+    if (usNum > 1000 && usNum < 10000)
+    {
+        int digit1 = usNum % 1000;
+        int digit2 = digit1 % 100;
+        int digit3 = digit2 / 10;
+        int newNum = digit3;
+        Console.WriteLine($"Result is {newNum}");
+    }
+    else Console.WriteLine("We haven't third Num MY LORD");
+
+
+}
+ShowThirdNumb ();
+*/
+
+//ВЫХОДНЫЕЕЕЕ
+/*
+void TheWeeknd()
+{
+    Console.WriteLine("Enter the day of the week ");
+    int weekDay = Convert.ToInt32(Console.ReadLine());
+    if (weekDay >=1 && weekDay<=5)
+    {
+        Console.WriteLine("Cry and work");
+    }
+    else if (weekDay >=6 && weekDay <=7)
+    {
+        Console.WriteLine("You can relax SLAVE");
+    }
+    
+}
+TheWeeknd ();
+*/
